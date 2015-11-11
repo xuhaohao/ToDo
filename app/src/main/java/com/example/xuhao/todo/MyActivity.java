00f00464e9df0531ac49db74be4e13191f502809
xuhaohao <xuhaogogo@live.cn>
2015-11-11 16:35:30 +0800

    新内容

--- app/src/main/java/com/example/xuhao/todo/MyActivity.java
+++ app/src/main/java/com/example/xuhao/todo/MyActivity.java
@@ -1,13 +1,18 @@
 package com.example.xuhao.todo;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 
-public class MyActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyActivity extends AppCompatActivity implements NewItemFragment.OnNewItemAddedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,21 @@ public class MyActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        FragmentManager fm = getFragmentManager();
+        ToDoListFragment toDoListFragment = (ToDoListFragment)fm.findFragmentById(R.id.fgToDoList);
+        toDoItems = new ArrayList<String>();
+        aa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,toDoItems);
+        toDoListFragment.setListAdapter(aa);
     }
 
+    private List<String> toDoItems;
+    private ArrayAdapter<String> aa;
+
+
+    @Override
+    public void onOnNewItemAdded(String item) {
+        toDoItems.add(item);
+        aa.notifyDataSetChanged();
+    }
 }
---
+++ app/src/main/java/com/example/xuhao/todo/NewItemFragment.java
@@ -0,0 +1,93 @@
+package com.example.xuhao.todo;
+
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NewItemFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NewItemFragment extends Fragment {
+
+    private static final String TAG = "NewItemFragment";
+    private static final String Name = "name";
+
+    private String tag;
+    private String name;
+
+
+    public NewItemFragment() {
+        // Required empty public constructor
+    }
+
+    private OnNewItemAddedListener listener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        listener = (OnNewItemAddedListener)context;
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param tag Parameter 1.
+     * @param name Parameter 2.
+     * @return A new instance of fragment NewItemFragment.
+     */
+    public static NewItemFragment newInstance(String tag, String name) {
+        NewItemFragment fragment = new NewItemFragment();
+        Bundle args = new Bundle();
+        args.putString(TAG, tag);
+        args.putString(Name, name);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            tag = getArguments().getString(TAG);
+            name = getArguments().getString(Name);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_new_item2, container, false);
+        final EditText evNewItem = (EditText) view.findViewById(R.id.etNewItem);
+        evNewItem.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getAction() == KeyEvent.ACTION_DOWN){
+                    if(keyCode == KeyEvent.KEYCODE_DPAD_CENTER || keyCode == KeyEvent.KEYCODE_ENTER){
+                        listener.onOnNewItemAdded(evNewItem.getText().toString());
+                        evNewItem.setText("");
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        return view;
+    }
+
+    public interface OnNewItemAddedListener{
+        void onOnNewItemAdded(String item);
+    }
+
+
+}
