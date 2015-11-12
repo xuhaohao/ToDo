1e3cefa593ddfb7ddbd98120fe745f1995c5326b
xuhaohao <xuhaogogo@live.cn>
2015-11-12 15:29:34 +0800

    调整

--- app/src/main/java/com/example/xuhao/todo/CompassView.java
+++ app/src/main/java/com/example/xuhao/todo/CompassView.java
@@ -1,8 +1,12 @@
 package com.example.xuhao.todo;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
 
 /**
  * Created by xuhao on 2015/11/11.
@@ -23,11 +27,41 @@ public class CompassView extends View {
         init();
     }
 
-    private void init(){
+    private void init() {
         this.setFocusable(true);
+
+        Resources r = getResources();
+
+        circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        circlePaint.setColor(r.getColor(R.color.background_color));
+        circlePaint.setStrokeWidth(1);
+        circlePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        strNorth = r.getString(R.string.cardinal_north);
+        strSouth = r.getString(R.string.cardinal_south);
+        strEast = r.getString(R.string.cardinal_east);
+        strWest = r.getString(R.string.cardinal_west);
+
+        textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        textPaint.setColor(r.getColor(R.color.text_color));
+
+        textHeight = (int) textPaint.measureText("yY");
+
+        markerPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        markerPaint.setColor(r.getColor(R.color.background_color));
     }
 
     private float bearing;
+
+    private Paint markerPaint;
+    private Paint textPaint;
+    private Paint circlePaint;
+    private int textHeight;
+    private String strNorth;
+    private String strSouth;
+    private String strEast;
+    private String strWest;
+
 
     public float getBearing() {
         return bearing;
@@ -35,6 +69,7 @@ public class CompassView extends View {
 
     public void setBearing(float bearing) {
         this.bearing = bearing;
+        sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED);
     }
 
     @Override
@@ -42,9 +77,9 @@ public class CompassView extends View {
         int measureWidth = measure(widthMeasureSpec);
         int measureHeight = measure(heightMeasureSpec);
 
-        int minValue = Math.min(measureWidth,measureHeight);
+        int minValue = Math.min(measureWidth, measureHeight);
 
-        setMeasuredDimension(minValue,minValue);
+        setMeasuredDimension(minValue, minValue);
     }
 
     private int measure(int measureSpec) {
@@ -52,11 +87,82 @@ public class CompassView extends View {
         int specMode = MeasureSpec.getMode(measureSpec);
         int specSize = MeasureSpec.getSize(measureSpec);
 
-        if (specMode == MeasureSpec.UNSPECIFIED){
+        if (specMode == MeasureSpec.UNSPECIFIED) {
             result = 200;
-        }else{
+        } else {
             result = specSize;
         }
         return result;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        int height = getMeasuredHeight();
+        int width = getMeasuredWidth();
+
+        int px = width / 2;
+        int py = height / 2;
+
+        int radius = Math.min(px, py);
+
+        canvas.drawCircle(px, py, radius, circlePaint);
+        canvas.save();
+        canvas.rotate(-bearing, px, py);
+
+        int textWidth = (int) textPaint.measureText("W");
+        int cardinalX = px - textWidth / 2;
+        int cardinalY = py - radius + textHeight;
+
+        for (int i = 0; i < 24; i++) {
+            canvas.drawLine(px, py - radius, px, py - radius + 10, markerPaint);
+
+            canvas.save();
+            canvas.translate(0,textHeight);
+
+           if (i % 6 == 0){
+               String dirString = "";
+               switch(i){
+                   case(0):{
+                       dirString = strNorth;
+                       int arrowY = 2*textHeight;
+                       canvas.drawLine(px,arrowY,px-5,3*textHeight,markerPaint);
+                       canvas.drawLine(px,arrowY,px+5,3*textHeight,markerPaint);
+                       break;
+                   }
+                   case(6):dirString = strEast;break;
+                   case(12):dirString = strSouth;break;
+                   case(18):dirString = strWest;break;
+               }
+            canvas.drawText(dirString,cardinalX,cardinalY,textPaint);
+           }
+            else if(i % 3 == 0){
+               String angle = String.valueOf(i*15);
+               float angleTextWidth = textPaint.measureText(angle);
+
+               int angleTextX = (int)(px-angleTextWidth/2);
+               int angleTextY = py-radius+textHeight;
+               canvas.drawText(angle,angleTextX,angleTextY,textPaint);
+           }
+            canvas.restore();
+
+            canvas.rotate(15,px,py);
+        }
+        canvas.restore();
+    }
+
+    @Override
+    public boolean dispatchPopulateAccessibilityEvent(AccessibilityEvent event) {
+        super.dispatchPopulateAccessibilityEvent(event);
+        if (isShown()){
+            String bearingStr = String.valueOf(bearing);
+            if (bearingStr.length() > AccessibilityEvent.MAX_TEXT_LENGTH){
+                bearingStr = bearingStr.substring(0,AccessibilityEvent.MAX_TEXT_LENGTH);
+
+                event.getText().add(bearingStr);
+                return true;
+            }
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/xuhao/todo/NewItemFragment.java
+++ app/src/main/java/com/example/xuhao/todo/NewItemFragment.java
@@ -67,7 +67,7 @@ public class NewItemFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_new_item2, container, false);
+        View view = inflater.inflate(R.layout.fragment_new_item, container, false);
         final EditText evNewItem = (EditText) view.findViewById(R.id.etNewItem);
         evNewItem.setOnKeyListener(new View.OnKeyListener() {
             @Override
